c7bb933707f4652e383558f7340b03bb11432146
Przemysław Pogorzelec <pogosoftware@gmail.com>
2019-05-04 21:30:13 +0200

    Add Event Database Contracts. #vfwmj[closed]

---
+++ roknoe-core/src/Roknoe.Core.Contracts/Databases/Events/Agenda.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Roknoe.Core.Contracts.Databases.Events
+{
+    public class Agenda
+    {
+        public DateTime Timeline { get; set; }
+        public string Title { get; set; }
+    }
+}
